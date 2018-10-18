4af75d145c788922601773a4dcd6b3b56663617a
Sean Hong <iamseanhong@gmail.com>
2018-10-17 21:47:55 -0700

    Create ProjectSummary to make code more moduler

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import _ from 'lodash';
 
+import ProjectSummary from './ProjectSummary';
+
 const ProjectList = () => {
   return (
-    <div>
-      {_.range(3).map(idx => {
-        return (
-          <div key={idx} className="project-list section">
-            <div className="card z-depth-0 proejct-summary">
-              <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
-                <p>Posted by the Net Ninja</p>
-                <p className="grey-text">17th October, 9pm</p>
-              </div>
-            </div>
-          </div>
-        );
+    <div className="project-list section">
+      {_.range(4).map(idx => {
+        return <ProjectSummary key={idx} />;
       })}
     </div>
   );
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 proejct-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the Net Ninja</p>
+        <p className="grey-text">17th October, 9pm</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
