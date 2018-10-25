7ce3dd55c01801e1136ed6f7135fb130438f39ab
Sean Hong <iamseanhong@gmail.com>
2018-10-24 20:32:13 -0700

    Add style

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const ProjectSummary = () => {
   return (
-    <div className="card z-depth-0 proejct-summary">
+    // <div className="card z-depth-0 proejct-summary">
+    <div className="card proejct-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">Project Title</span>
         <p>Posted by the Net Ninja</p>
