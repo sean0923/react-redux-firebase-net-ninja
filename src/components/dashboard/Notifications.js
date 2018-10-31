1bc3effa17b3ccae75e21d198a88f609529ad0fe
Sean Hong <iamseanhong@gmail.com>
2018-10-30 20:04:14 -0700

    Create first cloud func

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Notifications = () => {
   return (
-    <div>
-      <p>Notification</p>
+    <div className="section">
+      <div className="card">
+        <div className="card-content">
+          <div className="card-title">Notification</div>
+          <ul className="notification">
+            <li>notification</li>
+            <li>notification</li>
+            <li>notification</li>
+            <li>notification</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/reducers/store.js
+++ src/reducers/store.js
@@ -29,7 +29,7 @@ const composeWithArgs = getComposeWithArgs(
     userProfile: 'users',
     attachAuthIsReady: true,
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : null
 );
 
 const store = createStore(rootReducer, {}, composeWithArgs);
