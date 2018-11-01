49358dfbdcf049ee88f464b1c33b9cab3f61282f
Sean Hong <iamseanhong@gmail.com>
2018-10-31 20:49:25 -0700

    Able to display notification

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { firestoreConnect } from 'react-redux-firebase';
 
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
 
 class Dashboard extends Component {
   render() {
+    const { notifications } = this.props;
     return (
       <div>
         <div className="dashboard container">
@@ -13,7 +17,7 @@ class Dashboard extends Component {
               <ProjectList />
             </div>
             <div className="col s12 m5 offset-m1">
-              <Notifications />
+              <Notifications notifications={notifications} />
             </div>
           </div>
         </div>
@@ -21,5 +25,11 @@ class Dashboard extends Component {
     );
   }
 }
+const mapStateToProps = state => {
+  return { notifications: state.firestore.ordered.notifications };
+};
 
-export default Dashboard;
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'notifications', limit: 3 }])
+)(Dashboard);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import moment from 'moment';
 
-const Notifications = () => {
+const Notifications = ({ notifications }) => {
   return (
     <div className="section">
       <div className="card">
         <div className="card-content">
           <div className="card-title">Notification</div>
           <ul className="notification">
-            <li>notification</li>
-            <li>notification</li>
-            <li>notification</li>
-            <li>notification</li>
+            {notifications &&
+              notifications.map(item => {
+                const { id, content, user, time } = item;
+                return (
+                  <div key={id}>
+                    <div>
+                      <span className="pink-text">{user} </span>
+                      <span>{content}</span>
+                    </div>
+                    <div className="grey-text">{moment(time.toDate()).fromNow()}</div>
+                  </div>
+                );
+              })}
           </ul>
         </div>
       </div>
